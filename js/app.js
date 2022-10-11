68f044b469c74bf2c6062877cc86d4c70439eb60
ahmedmishmael <ahmedm.ishmael@outlook.com>
2022-10-11 14:49:07 +0200

    Version 1.0

--- js/app.js
+++ js/app.js
@@ -1,17 +1,29 @@
-const aboutBtn = document.querySelector(`#about-btn`);
-const portfolioBtn = document.querySelector(`#portfolio-btn`);
-const servicesBtn = document.querySelector(`#services-btn`);
-const contactBtn = document.querySelector(`#contact-btn`);
-const about = document.querySelector(`#about`);
-const portfolio = document.querySelector(`#portfolio`);
-const services = document.querySelector(`#services`);
-const contact = document.querySelector(`#contact`);
+// const aboutBtn = document.querySelector(`#about-btn`);
+// const portfolioBtn = document.querySelector(`#portfolio-btn`);
+// const contactBtn = document.querySelector(`#contact-btn`);
+// const about = document.querySelector(`#about`);
+// const portfolio = document.querySelector(`#portfolio`);
+// const contact = document.querySelector(`#contact`);
 
-const buttons = [aboutBtn, portfolioBtn, servicesBtn, contactBtn];
-const sections = [about, portfolio, services, contact];
+// const buttons = [aboutBtn, portfolioBtn, servicesBtn, contactBtn];
+// const sections = [about, portfolio, services, contact];
 
-for (let index = 0; index < buttons.length; index++) {
-  buttons[index].addEventListener("click", () => {
-    sections[index].scrollIntoView();
-  });
-}
+// for (let index = 0; index < buttons.length; index++) {
+//   buttons[index].addEventListener("click", () => {
+//     sections[index].scrollIntoView();
+//   });
+// }
+
+const selectedBtn = document.getElementById(`selected-btn`);
+const allBtn = document.getElementById(`all-btn`);
+const selected = document.getElementById(`selected`);
+const all = document.getElementById(`all`);
+
+selectedBtn.addEventListener("click", () => {
+  all.style.display = `none`;
+  selected.style.display = `flex`;
+});
+allBtn.addEventListener("click", () => {
+  selected.style.display = `none`;
+  all.style.display = `flex`;
+});
